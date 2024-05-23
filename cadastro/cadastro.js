c61c3e7762537586894e32ae46c25beb74d95209
Marcelo Lima <marcelolima.ss2310@outlook.com>
2024-05-22 21:22:15 -0300

    a

--- cadastro/cadastro.js
+++ cadastro/cadastro.js
@@ -1,19 +1,17 @@
-document.getElementById('cadastroForm').addEventListener('submit', function(event) {
+document.getElementById('cadastroForm').addEventListener('submit', async function(event) {
     event.preventDefault(); // Evita o envio padrão do formulário
 
     const nomeUsuario = document.getElementById('nomeUsuario').value;
     const emailUsuario = document.getElementById('emailUsuario').value;
     const senhaUsuario = document.getElementById('senhaUsuario').value;
-    const categoriaMusical = document.getElementById('categoriaMusical').value;
 
     const novoUsuario = {
         nomeUsuario,
         emailUsuario,
-        senhaUsuario,
-        categoriaMusical
-    };
+        senhaUsuario
+    }
 
-    fetch('/usuarios/cadastrar', {
+    await fetch('https://localhost:3000/usuarios/cadastrar', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
